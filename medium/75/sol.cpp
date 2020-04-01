57ec1b082dc091d49b832ea80ee327949e9c5649
aditya pratap singh <pratapaditya1997@gmail.com>
2020-04-01 23:01:06 +0530

    75

---
+++ medium/75/sol.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int l=0, r=n-1;
+        for(int i=0; i<=r; ) {
+            if(nums[i] == 0) {
+                swap(nums[i], nums[l]);
+                l++;
+                i++;
+            } else if(nums[i] == 2) {
+                swap(nums[i], nums[r]);
+                r--;
+            } else {
+                i++;
+            }
+        }
+    }
+};
