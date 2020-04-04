2e28cbed3ffedf3f071dd180183e21af44d03544
aditya pratap singh <pratapaditya1997@gmail.com>
2020-04-04 18:29:25 +0530

    328

---
+++ medium/328/sol.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == nullptr || head->next == nullptr) return head;
+        ListNode *odd = head, *even = head->next, *evenHead = head->next; 
+        while(even != nullptr && even->next != nullptr) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
