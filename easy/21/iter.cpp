302fb49ea15dadab583e82b82b5d5612cf5f9132
aditya pratap singh <pratapaditya1997@gmail.com>
2020-06-25 01:37:54 +0530

    21 iter updatee

--- easy/21/iter.cpp
+++ easy/21/iter.cpp
@@ -1,28 +1,12 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         if(l1 == nullptr) return l2;
         if(l2 == nullptr) return l1;
         
-        ListNode *head;
-        if(l1->val <= l2->val) {
-            head = l1;
-            l1 = l1->next;
-        }
-        else {
-            head = l2;
-            l2 = l2->next;
-        }
-        
+        ListNode *head = new ListNode(0);
         ListNode *temp = head;
+        
         while(l1 != nullptr && l2 != nullptr) {
             if(l1->val <= l2->val) {
                 temp->next = l1;
@@ -34,17 +18,8 @@ public:
             temp = temp->next;
         }
         
-        while(l1 != nullptr) {
-            temp->next = l1;
-            l1 = l1->next;
-            temp = temp->next;
-        }
-        while(l2 != nullptr) {
-            temp->next = l2;
-            l2 = l2->next;
-            temp = temp->next;
-        }
-        
-        return head;
+        if(l1 != nullptr) temp->next = l1;
+        if(l2 != nullptr) temp->next = l2;
+        return head->next;
     }
 };
