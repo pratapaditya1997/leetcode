d824b7ee20cb4f59729b67cf4ee8cba54f6675f4
aditya pratap singh <pratapaditya1997@gmail.com>
2020-06-25 12:54:35 +0530

    23 sol updated

--- hard/23/sol.cpp
+++ hard/23/sol.cpp
@@ -1,16 +1,5 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
 struct comp {
-    bool operator()(const ListNode* l, ListNode* r) {
+    bool operator()(const ListNode* l, const ListNode* r) {
         return l->val > r->val;
     }
 };
@@ -20,7 +9,7 @@ public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         ListNode* head = new ListNode();
         priority_queue<ListNode*, vector<ListNode*>, comp> pq;
-        for(auto node: lists) {
+        for(auto& node: lists) {
             if(node != nullptr) pq.push(node);
         }
         
