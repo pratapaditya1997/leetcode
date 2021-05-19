a6673005ed30ab04e6350f92c7ed3936e5f2ae6f
aditya pratap singh <pratapaditya1997@gmail.com>
2021-05-19 23:54:25 +0530

    947 most stones removed
    dsu based very easy solution

---
+++ medium/947/sol.cpp
@@ -0,0 +1,57 @@
+typedef pair<int,int> pii;
+
+class dsu {
+public:
+    vector<int> p, sz;
+    int n;
+    
+    dsu(int _n): n(_n) {
+        p.resize(n);
+        sz.assign(n, 1);
+        iota(p.begin(), p.end(), 0);
+    }
+    
+    int get(int x) {
+        return (x == p[x] ? x : (p[x] = get(p[x])));
+    }
+    
+    void unite(int x, int y) {
+        x = get(x);
+        y = get(y);
+        if (x != y) {
+            if (sz[x] < sz[y]) swap(x, y);
+            p[y] = x;
+            sz[x] += sz[y];
+        }
+    }
+};
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        
+        dsu d(n);
+        for (int i=0; i<n; i++) {
+            pii pi = {stones[i][0], stones[i][1]};
+            for (int j=0; j<i; j++) {
+                pii pj = {stones[j][0], stones[j][1]};
+                if (pi.first == pj.first || pi.second == pj.second) {
+                    d.unite(i, j);
+                }
+            }
+        }
+        
+        set<int> s;
+        int ans = 0;
+        for (int i=0; i<n; i++) {
+            int par = d.get(i);
+            if (s.find(par) == s.end()) {
+                s.insert(par);
+                ans += (d.sz[par] - 1);
+            }
+        }
+        
+        return ans;
+    }
+};
