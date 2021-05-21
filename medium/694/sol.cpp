dc5c810471c58d0332a6158b62509b5f37e460c2
aditya pratap singh <pratapaditya1997@gmail.com>
2021-05-21 16:56:49 +0530

    Create sol.cpp
    hash based optimized solution

---
+++ medium/694/sol.cpp
@@ -0,0 +1,42 @@
+typedef pair<int, int> pii;
+
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
+char dir[4] = {'L','R','U','D'};
+
+bool valid(int x, int y, vector<vector<int>>& g) {
+    int n = g.size(), m = g[0].size();
+    return (x >= 0 && y >= 0 && x < n && y < m && g[x][y]);
+}
+
+void dfs(int x, int y, vector<vector<int>>& g, string& cur) {
+    vis[x][y] = 1;
+    for (int i=0; i<4; i++) {
+        int nx = x + dx[i], ny = y + dy[i];
+        if (valid(nx, ny, g) && !vis[nx][ny]) {
+            cur += dir[i];
+            dfs(nx, ny, g, vis, cur);
+        }
+    }
+    cur += '0';
+}
+
+class Solution {
+public:
+    int numDistinctIslands(vector<vector<int>>& g) {
+        int n = g.size(), m = g[0].size();
+        unordered_set<string> ans;
+        
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                if (!vis[i][j] && g[i][j]) {
+                    string cur = "";
+                    dfs(i, j, g, cur);
+                    ans.insert(cur);
+                }
+            }
+        }
+        
+        return ans.size();
+    }
+};
