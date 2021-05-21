8071b9549d8bf2fe4416db1d4b71600acd845ec7
aditya pratap singh <pratapaditya1997@gmail.com>
2021-05-21 13:37:24 +0530

    Update sol.cpp

--- medium/200/sol.cpp
+++ medium/200/sol.cpp
@@ -1,35 +1,34 @@
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
+
 class Solution {
 public:
-    int dx[4] = {0,0,-1,1};
-    int dy[4] = {-1,1,0,0};
-    
-    int n,m;
-    
-    void dfs(int x, int y, vector<vector<char>>& grid) {
-        grid[x][y] = '0';
-        for(int i=0; i<4; i++) {
-            int nx = x+dx[i], ny = y+dy[i];
-            if(nx>=0 && nx<n && ny>=0 && ny<m && grid[nx][ny] == '1') {
-                dfs(nx, ny, grid);
-            }
-        }
-    }
-    
-    int numIslands(vector<vector<char>>& grid) {
-        n = grid.size();
-        if(n == 0) return 0;
+    int numIslands(vector<vector<char>>& g) {
+        int n = g.size(), m = g[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
         
-        m = grid[0].size();
+        auto valid = [&](int x, int y) {
+            return (x >= 0 && y >= 0 && x < n && y < m && g[x][y] == '1');
+        };
+        
+        function<void(int, int)> dfs = [&](int x, int y) {
+            vis[x][y] = 1;
+            for (int i=0; i<4; i++) {
+                int nx = x + dx[i], ny = y + dy[i];
+                if (valid(nx, ny) && !vis[nx][ny]) dfs(nx, ny);
+            }
+        };
         
         int ans = 0;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(grid[i][j] == '1') {
-                    ans++;
-                    dfs(i,j,grid);
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                if (!vis[i][j] && g[i][j] == '1') {
+                    ++ans;
+                    dfs(i, j);
                 }
             }
         }
+        
         return ans;
     }
 };
