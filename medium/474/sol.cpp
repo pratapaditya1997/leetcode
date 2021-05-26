19f6c4c43b64935d27ed3869e97ccdf74736c0a0
aditya pratap singh <pratapaditya1997@gmail.com>
2021-05-27 01:59:12 +0530

    474 ones and zeroes
    3D DP based solution

---
+++ medium/474/sol.cpp
@@ -0,0 +1,42 @@
+typedef pair<int, int> pii;
+
+class Solution {
+public:
+    pii GetCounts(const string& str) {
+        int z = 0, o = 0;
+        for (char c: str) {
+            if (c == '0') ++z;
+            else ++o;
+        }
+        return {z, o};
+    }
+    
+    int findMaxForm(vector<string>& strs, int W0, int W1) {
+        int n = strs.size();
+        vector<int> zero_w(n + 1, 0), one_w(n + 1, 0);
+        
+        for (int i=1; i<=n; ++i) {
+            pii cnt = GetCounts(strs[i-1]);
+            zero_w[i] = cnt.first;
+            one_w[i] = cnt.second;
+        }
+        
+        int dp[n + 1][W0 + 1][W1 + 1];
+        memset(dp, 0, sizeof dp);
+        
+        for (int i=1; i<=n; ++i) {
+            for (int j=0; j <= W0; ++j) {
+                for (int k=0; k <= W1; ++k) {
+                    // skip current one
+                    dp[i][j][k] = dp[i-1][j][k];
+                    
+                    // take current one
+                    if (j - zero_w[i] < 0 || k - one_w[i] < 0) continue;
+                    dp[i][j][k] = max(dp[i][j][k], dp[i-1][j - zero_w[i]][k - one_w[i]] + 1);
+                }
+            }
+        }
+        
+        return dp[n][W0][W1];
+    }
+};
