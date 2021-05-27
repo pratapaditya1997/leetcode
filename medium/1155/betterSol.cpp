cadd71098129c076faf2078993971382f295e2d4
aditya pratap singh <pratapaditya1997@gmail.com>
2021-05-28 01:41:36 +0530

    better code quality

--- medium/1155/betterSol.cpp
+++ medium/1155/betterSol.cpp
@@ -1,28 +1,34 @@
+typedef long long int ll;
+
+const ll MOD = 1e9 + 7;
+
+inline ll Add(ll x, ll y) {
+    x += y;
+    if (x >= MOD) x -= MOD;
+    return x;
+}
+
+inline ll Sub(ll x, ll y) {
+    x -= y;
+    if (x < 0) x += MOD;
+    return x;
+}
+
 class Solution {
 public:
-    int mod = 1e9+7;
-    
-    void add(int& a, int b) {
-        a += b;
-        if(a >= mod) a -= mod;
-    }
-    
-    void sub(int& a, int b) {
-        a -= b;
-        if(a<0) a += mod;
-    }
-    
-    int numRollsToTarget(int d, int f, int target) {
-        vector<vector<int>> dp(d+1, vector<int>(target+1,0));
+    int numRollsToTarget(int n, int f, int target) {
+        vector<vector<ll>> dp(n + 1, vector<ll>(target + 1, 0));
         dp[0][0] = 1;
-        for(int i=1; i<=d; i++) {
-            int cur = 0;
-            for(int j=1; j<=target; j++) {
-                add(cur,dp[i-1][j-1]);
-                if(j>f)  sub(cur,dp[i-1][j-f-1]);
+        
+        for (int i=1; i <= n; ++i) {
+            ll cur = 0;
+            for (int j=1; j <= target; ++j) {
+                cur = Add(cur, dp[i-1][j-1]);
+                if (j-f-1 >= 0) cur = Sub(cur, dp[i-1][j-f-1]);
                 dp[i][j] = cur;
             }
         }
-        return dp[d][target];
+        
+        return dp[n][target];
     }
 };
