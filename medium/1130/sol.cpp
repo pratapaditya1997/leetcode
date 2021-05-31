ee37fd72c1f75bedd2fd67d3e31a494d2230e33e
aditya pratap singh <pratapaditya1997@gmail.com>
2021-06-01 01:25:33 +0530

    iterative

--- medium/1130/sol.cpp
+++ medium/1130/sol.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    int mctFromLeafValues(vector<int>& arr) {
-        int n = arr.size();
-        vector<vector<int>> dp(n, vector<int>(n,0)), maxi(n, vector<int>(n,0));
+    int mctFromLeafValues(vector<int>& a) {
+        int n = a.size();
         
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                int cur = 0;
-                for(int k=i; k<=j; k++) cur = max(cur,arr[k]);
-                maxi[i][j] = cur;
+        vector<vector<int>> dp(n, vector<int>(n, 0)),
+                            maxi(n, vector<int>(n, 0));
+        
+        for (int i=0; i<n; ++i) {
+            int val = a[i];
+            for (int j=i; j<n; ++j) {
+                val = max(val, a[j]);
+                maxi[i][j] = val;
             }
         }
         
-        for(int len=2; len<=n; len++) {
-            for(int i=0; i<n-len+1; i++) {
-                int j = i+len-1, best = INT_MAX;
-                for(int k=i; k<j; k++) {
-                    int cur = maxi[i][k]*maxi[k+1][j] + dp[i][k] + dp[k+1][j];
-                    best = min(best, cur);
+        for (int l=2; l<=n; ++l) {
+            for (int i=0; i+l-1 < n; ++i) {
+                int j = i+l-1, val = INT_MAX;
+                for (int k=i; k<j; ++k) {
+                    val = min(val, dp[i][k] + dp[k+1][j] + maxi[i][k] * maxi[k+1][j]);
                 }
-                dp[i][j] = best;
+                dp[i][j] = val;
             }
         }
+        
         return dp[0][n-1];
     }
 };
