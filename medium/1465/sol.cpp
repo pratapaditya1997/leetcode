9545934759deb2712a69fa7c1a3bc8f15eade5c5
aditya pratap singh <pratapaditya1997@gmail.com>
2021-06-03 14:47:48 +0530

    Update sol.cpp

--- medium/1465/sol.cpp
+++ medium/1465/sol.cpp
@@ -1,27 +1,30 @@
-using ll = long long int;
-const int mod = 1e9+7;
+typedef long long int ll;
+
+const int MOD = 1e9 + 7;
+
+inline int Mul(ll x, ll y) {
+    return ((x % MOD) * (y % MOD)) % MOD;
+}
 
 class Solution {
 public:
-    int mul(ll a, ll b) {
-        ll res = ((a%mod) * (b%mod)) % mod;
-        return res;
-    }
-    
-    int maxArea(int h, int w, vector<int>& a, vector<int>& b) {
-        a.push_back(h);
-        b.push_back(w);
-        int n = a.size(), m = b.size();
+    int maxArea(int h, int w, vector<int>& hc, vector<int>& vc) {
+        hc.push_back(0); hc.push_back(h);
+        vc.push_back(0); vc.push_back(w);
         
-        sort(a.begin(), a.end());
-        sort(b.begin(), b.end());
-        int maxi_width = b[0], maxi_height = a[0];
-        for(int i=1; i<n; i++) {
-            maxi_height = max(maxi_height, a[i] - a[i-1]);
+        sort(hc.begin(), hc.end());
+        sort(vc.begin(), vc.end());
+        
+        int max_h = 0, max_v = 0;
+        int h_len = hc.size(), v_len = vc.size();
+        
+        for (int i=0; i<h_len-1; ++i) {
+            max_h = max(max_h, hc[i+1] - hc[i]);
         }
-        for(int i=1; i<m; i++) {
-            maxi_width = max(maxi_width, b[i] - b[i-1]);
+        for (int i=0; i<v_len-1; ++i) {
+            max_v = max(max_v, vc[i+1] - vc[i]);
         }
-        return mul(maxi_height, maxi_width);
+        
+        return Mul(max_h, max_v);
     }
 };
