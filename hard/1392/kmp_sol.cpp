54d6f1d0f0c6c5e27b752c36e9a98402373e0711
aditya pratap singh <pratapaditya1997@gmail.com>
2021-06-03 20:40:06 +0530

    1392 kmp

---
+++ hard/1392/kmp_sol.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string longestPrefix(string s) {
+        int n = s.length();
+        vector<int> pi(n, 0);
+        
+        for (int i=1; i<n; ++i) {
+            int j = pi[i-1];
+            while (j>0 && s[i] != s[j]) {
+                j = pi[j-1];
+            }
+            if (s[i] == s[j]) ++j;
+            pi[i] = j;
+        }
+        
+        return s.substr(0, pi[n-1]);
+    }
+};
